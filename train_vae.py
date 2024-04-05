1b5d3d624a11bc28d5b4bac3b03678278600dace
jt292 <jacob.thoenes@uni-rostock.de>
2024-04-05 12:37:37 +0200

    1st vae training

--- train_vae.py
+++ train_vae.py
@@ -7,6 +7,7 @@ from tensorflow.keras.optimizers import Adam
 from src.vae import vae_model
 
 boundary = Boundary()
+ball = BallAnomaly(x=1,y=1,z=1,r=5,γ=1)
 
 X_all_anomalys = list()
 radius_labels = list()
